2e6bb4b542466225907d38674c271700befa80a6
Jay Lansiquot <jay.lansiquot@wealthsimple.com>
2026-04-29 21:13:16 -0400

    feat(nvim): enable tokyonight transparency to match ghostty

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 return {
-  { "folke/tokyonight.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
